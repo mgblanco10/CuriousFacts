cea8f2453a8279007563d9f3bb36f4ecb0f4a8b3
mgblanco10 <mgblanco1001@gmail.com>
2023-06-06 17:21:22 +0200

    wip function of addFavoriteMessage and getNewMessage

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,4 @@
-
-let messageList = [];
-
-function loadMessages() {
-  fetch('https://uselessfacts.jsph.pl/random.json?language=en')
-    .then(response => response.json())
-    .then(data => {
-      messageList.push(data.text);
-      renderMessageList();
-    })
-    .catch(error => {
-      console.error('Error al cargar los mensajes:', error);
-    });
-}
-
-function renderMessageList() {
-  let messageListElement = document.getElementById('message-list');
-  messageListElement.innerHTML = ''; 
-  messageList.forEach(message => {
-    let li = document.createElement('li');
-    li.textContent = message;
-    messageListElement.appendChild(li);
-  });
-}
+let favoriteMessageList = [];
 
 function getNewMessage() {
   fetch('https://uselessfacts.jsph.pl/random.json?language=en')
@@ -29,17 +6,45 @@ function getNewMessage() {
     .then(data => {
       let currentMessage = document.getElementById('current-message');
       currentMessage.textContent = data.text;
-    
-      messageList.push(data.text);
-      renderMessageList();
+      currentMessage.dataset.message = data.text; 
+
+      let starImage = document.createElement('img');
+      starImage.src = '../img/animateStar.png';
+      starImage.alt = 'Estrella';
+      starImage.className = 'favorite-star';
+      starImage.style.verticalAlign = 'middle';
+
+      let previousStarImage = document.querySelector('.favorite-star');
+      if (previousStarImage) {
+        previousStarImage.remove();
+      }
+
+      currentMessage.appendChild(starImage);
+
+      starImage.addEventListener('click', addFavoriteMessage);
     })
     .catch(error => {
       console.error('Error al obtener el mensaje:', error);
     });
 }
 
-document.getElementById('new-message-btn').addEventListener('click', function() {
-  getNewMessage();
-});
+function addFavoriteMessage(event) {
+  let message = event.target.parentNode.dataset.message;
+  if (message) {
+    let favoriteMessageListElement = document.getElementById('message-list');
+    let li = document.createElement('li');
 
-loadMessages();
+    let image = document.createElement('img');
+    image.src = '../img/remove.png';
+    image.alt = 'icono de remove';
+    image.className = 'remove';
+    image.style.verticalAlign = 'middle';
+
+    li.appendChild(image);
+    li.insertAdjacentText('beforeend', message);
+
+    favoriteMessageListElement.appendChild(li);
+  }
+}
+
+document.getElementById('new-message-btn').addEventListener('click', getNewMessage);
