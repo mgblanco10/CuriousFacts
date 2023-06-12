b50d08f2125a7cfb66288051ca5ec7f1bee3a3ca
Nelora Mood <lorenamoralesodena@gmail.com>
2023-06-12 10:59:41 +0200

    merge

--- scripts/functionGetNewMessage.js
+++ scripts/functionGetNewMessage.js
@@ -3,6 +3,7 @@ function getNewMessage() {
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
+      console.log(data);
       let currentMessage = document.getElementById('current-message');
       currentMessage.textContent = data.text;
       currentMessage.dataset.message = data.text; 
@@ -12,7 +13,7 @@ function getNewMessage() {
       starImage.alt = 'Estrella que al presionar agrega a la lista de favoritos';
       starImage.className = 'favorite-star';
       starImage.style.verticalAlign = 'middle';
-
+      // console.log(starImage);
       let previousStarImage = document.querySelector('.favorite-star');
       if (previousStarImage) {
         previousStarImage.remove();
